786bc1d4ad272f73fb0120e520aa88374388b402
Magnus <magn902m@stud.kea.dk>
2022-02-19 12:02:05 +0100

    added random color on reload

--- script.js
+++ script.js
@@ -25,8 +25,25 @@ function setup() {
   // console.log(HTML);
 }
 
+// --- Create random color ---
+function calcRandom(min, max) {
+  const num = Math.floor(Math.random() * (max - min)) + min;
+  return num;
+}
+
+function randomColor() {
+  return "rgb(" + calcRandom(0, 255) + ", " + calcRandom(0, 255) + ", " + calcRandom(0, 255) + ")";
+}
+
+function setRandomColor() {
+  const newColor = randomColor();
+  HTML.colorBox.style.setProperty("background-color", newColor);
+  HTML.input.style.setProperty("value", newColor);
+}
+
 // ***** Model *****
 function getColor() {
+  setRandomColor();
   HTML.input.addEventListener("input", (elm) => {
     let inputColorValue = HTML.input.value;
     let harmonyValue = HTML.harmony.value;
@@ -159,6 +176,49 @@ function rgbToHSL(rgbObj) {
   return hslObj;
 }
 
+function hslToRGB(hslObj) {
+  h = h;
+  s = s / 100;
+  l = l / 100;
+
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+    x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+    m = l - c / 2,
+    r = 0,
+    g = 0,
+    b = 0;
+  if (0 <= h && h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (60 <= h && h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (240 <= h && h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  } else if (300 <= h && h < 360) {
+    r = c;
+    g = 0;
+    b = x;
+  }
+  r = Math.round((r + m) * 255);
+  g = Math.round((g + m) * 255);
+  b = Math.round((b + m) * 255);
+
+  return { r, g, b };
+}
+
 function displayColorBox(hexV) {
   HTML.colorBox.style.backgroundColor = `${hexV}`;
   //   HTML.colorBody.style.backgroundColor = `${hexV}`;
@@ -183,9 +243,12 @@ function displayHSL(hslV) {
   HTML.hslValue.textContent = `hsl: ${hslV.h}, ${hslV.s}, ${hslV.l}`;
 }
 
-function displayAnalogous() {
+function displayAnalogous(hslObj) {
   console.log("displayAnalogous");
+  let newHSLObj = calcAnalogous(hslObj);
 }
+
+function calcAnalogous() {}
 
 function displayMonochromatic() {
   console.log("displayMonochromatic");
